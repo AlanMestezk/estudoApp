b53be2c5ebb154a6e4d1f32e6522c7acca9429f0
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-08-08 15:58:19 -0300

    estudando encapsulamento com properties

---
+++ AppParaEstudoCSharp/Estudohihi.cs
@@ -0,0 +1,66 @@
+﻿
+
+namespace EstudoHehe {
+    internal class Estudohihi {
+
+        private string _nome;
+        private int _idade;
+        private double _altura;
+        private string _corFavorita;
+
+        public Estudohihi() {}
+
+        public Estudohihi(string nome, int idade, double altura) {
+            _nome = nome;
+            _idade = idade;
+            _altura = altura;
+        }
+
+        public Estudohihi(string nome, int idade, double altura, string corFavorita) : this(nome, idade, altura) {
+            _corFavorita = corFavorita;
+        }
+
+        public string Nome {
+            get { return _nome; }
+            
+            set {
+                if (value != null && value.Length > 4) {
+                    Console.WriteLine($"aceito");
+                }
+                else {
+                    Console.WriteLine($"O nome de usuário {_nome = value} é muito curto");
+                }
+            }
+        }
+
+        public int Idade {
+
+            set {
+                if(value != null && value > 18) {
+                    Console.WriteLine($"aceito");
+                }
+                else {
+                    Console.WriteLine($"{_idade = value} anos não aceito");
+                }
+            }
+
+        }
+
+        public double Altura {
+            get { return _altura; }
+            set {
+                Console.WriteLine("aceito");
+             }
+        }
+
+        public string CorFavorita {
+
+            get { return _corFavorita; }
+            set {
+                Console.WriteLine("aceito");
+            }
+
+        }
+
+    }
+}
---
+++ AppParaEstudoCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+
+namespace EstudoHehe {
+
+    class Program {
+        static void Main(string[] args) {
+
+            Estudohihi dados = new Estudohihi();
+
+            Console.Write("Digite um usuário com mais de 4 dígitos: ");
+            dados.Nome = Console.ReadLine();
+            Console.Write("Digite a sua idade (+18): ");
+            dados.Idade = (int.Parse(Console.ReadLine()));
+            Console.Write("Digite sua altura: ");
+            dados.Altura = double.Parse(Console.ReadLine());
+            Console.Write("Digite sua cor favorita: ");
+            dados.CorFavorita = Console.ReadLine();
+
+        }
+    }
+
+}
